6293192dfb4e3fb5b358304fcbb1942a7e8e2db6
gavin-lb <94984768+gavin-lb@users.noreply.github.com>
2026-06-28 01:18:01 +0100

    feat: update GuildMemberRemove handler to also close any open tickets

--- api/events/on/guild-member-remove.ts
+++ api/events/on/guild-member-remove.ts
@@ -9,15 +9,30 @@ export async function handle(...args: ClientEvents[typeof name]): Promise<void> 
   const [member] = args;
 
   logger.debug({ member }, "Member left server");
-  for (const application of await api.submittedApplications.findMany({
+
+  const applications = api.submittedApplications.findMany({
     filter: { ownerId: { equals: member.user.id }, status: { equals: "open" } },
-  })) {
+  });
+
+  const tickets = api.tickets.findMany({
+    filter: { ownerId: { equals: member.user.id }, status: { equals: "open" } },
+  });
+
+  for (const application of await applications) {
     logger.debug({ application }, "Member had open application");
 
     api.submittedApplications.deny(application.id, {
       name: "Manus Mortis Bot",
       reason: "User has left the server.",
       avatar: AVATAR_URL,
+    });
+  }
+
+  for (const ticket of await tickets) {
+    logger.debug({ ticket }, "Member had open ticket");
+
+    api.tickets.close(ticket.id, {
+      name: "Manus Mortis Bot",
     });
   }
 }
