ecb9b5331401d30f9edce18ea47a3c85eac2500d
gavin-lb <94984768+gavin-lb@users.noreply.github.com>
2026-08-20 18:44:17 +0100

    feat: add prune action for submitted applications that no longer belong to an application

---
+++ api/actions/pruneSubmittedApplications.ts
@@ -0,0 +1,37 @@
+import { logger } from "gadget-server";
+
+export const run: ActionRun = async ({ api }) => {
+  const applicationRecords = await api.application.findMany({
+    select: { id: true },
+  });
+
+  const applicationIds = new Set(applicationRecords.map(({ id }) => id));
+
+  let records = await api.submittedApplications.findMany({
+    first: 50,
+    select: {
+      id: true,
+      applicationId: true
+    },
+  });
+
+  do {
+    const toDelete = records
+      .filter(
+        ({ applicationId }) =>
+          !applicationId || !applicationIds.has(applicationId),
+      )
+      .map(({ id }) => id);
+
+    if (toDelete.length > 0) {
+      await api.submittedApplications.bulkDelete(toDelete);
+
+      logger.warn(
+        { toDelete, applicationIds, records },
+        `Deleted ${toDelete.length} submitted applications that no longer have a corresponding application.`,
+      );
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } while (records.hasNextPage && (records = await records.nextPage()));
+};
